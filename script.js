b63548ee959dfd2c8301742d77ef9f20d869ed8a
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-03-27 14:36:07 -0700

    Added text fields and nonfunctional 'Save' button, implemented horizontal scroll for overflow.

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 /*
     Danielle Smith
     script.js -- Adds functionality to index.html
-
 */
 
 
-
+const hours = ["12:00 AM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM", "10:00 PM", "11:00 PM"];
 
 function Day(day_name, day_type){
     this.day_name = day_name;
     this.day_type = day_type;
 };
 
-
-function Schedule(){
-
-}
 
 
 
@@ -26,29 +21,20 @@ console.log(days);
 
 
 for(let i = 0; i < days.length; i++){
-    // $("#days").append('<div class = "tile"><div class = "tile is-parent ' + days[i]["day_name"] + " " + days[i]["day_type"] + ' day"><div class = "content"><p class = "title">' + days[i]["day_name"] + '</p></div></div></div>');
-    // for(let j = 0; j < 24; j++){
-            
-    // }
-    console.log(days[i]);
     $("#days").append(build_html_string(days[i]));
 }
 
 
-
+//Helper function to build ugly html string to insert into index.html
 function build_html_string(day){
     result = '<div class = "tile"><div class = "tile is-parent ' + day["day_name"] + " " + day["day_type"] + ' day"><div class = "content"><p class = "title">' + day["day_name"] + '</p><div class = "tile is-ancestor"><div class = "tile"><div class = tile is-vertical is-24"><div class = "tile is-parent is-vertical">';
-    
-
     for(let i = 0; i < 24; i++){
-        result += '<div id ' + i + ' class = "tile is-child"> Test </div>';
-
+        result += '<div id ' + hours[i] + ' class = "tile is-child hour">' + hours[i] + '<nav class = "level"><div class = "level-left"><div class = "level-item"><form><input type = "text" id = ' + i + ' name = "appointment"> </div></div><div class = "level-right"><div class = "level-item"><p class = "button"> Save </p></div></div></nav></form></div>';
     }
-    
-    
     const end_string = '</div></div></div></div></div></div></div></div></div>';
-    result += end_string;
-    return result;
+    return result += end_string;
 }
+
+
 
 
